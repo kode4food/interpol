282e69be311db7d178c6b0cf2c2e25d9d0966090
Thomas Bradford <thom@kode4food.it>
2014-03-30 21:27:38 +0200

    docco-happy

--- browserify/parser.js
+++ browserify/parser.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Interpol (Templates Sans Facial Hair)
  * Licensed under the MIT License
  * see doc/LICENSE.md
@@ -8,10 +8,8 @@
 
 "use strict";
 
-/*
- * This module is used to collect the requirements for a parser-included
- * Browserify build.  It's of no interest to Node.js
- */
+// This module is used to collect the requirements for a parser-included
+// Browserify build.  It's of no interest to Node.js
 
 require('./standard');
 require('../lib/parser');
--- browserify/standard.js
+++ browserify/standard.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Interpol (Templates Sans Facial Hair)
  * Licensed under the MIT License
  * see doc/LICENSE.md
@@ -8,11 +8,9 @@
 
 "use strict";
 
-/*
- * This module is used to collect the requirements for a minimal
- * Browserify build.  It's of no interest to Node.js
- */
- 
+// This module is used to collect the requirements for a minimal
+// Browserify build.  It's of no interest to Node.js
+
 // Set the Interpol browser global
 window.$interpol = require('../lib/interpol');
 
